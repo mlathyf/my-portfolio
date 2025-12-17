c8a1b63002961c71433e167416a8c441d381be3e
mlathyf <muhassanlathyf@gmail.com>
2025-12-17 15:29:08 +0500

    Remove Home from Navbar

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,4 @@
 export const navLinks = [
-  { href: "#home", label: "Home" },
   { href: "#about", label: "About" },
   { href: "#projects", label: "Projects" },
   { href: "#contact", label: "Contact" },
